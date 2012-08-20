5cdf85231854ae9e08ddaa6a6cacc559aef63a05
hackromanux@gmail.com <hackromanux@gmail.com@851d154f-5051-faa0-1c04-ff979f6fe470>
2012-08-20 05:24:21 +0000

    Anderson

---
+++ ProyectoBolsaTrabajo/src/com/proyecto/negocio/GestionAdministrador.java
@@ -0,0 +1,10 @@
+package com.proyecto.negocio;
+
+public class GestionAdministrador {
+	
+	//Como administrador necesito registrar los puestos laborales 
+	public void registrarPuestosLaborables(){
+		
+	}
+
+}
