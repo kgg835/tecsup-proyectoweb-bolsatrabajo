a46e243481e8415e9f7d3c9b4f2db19374d2fb10
hackromanux@gmail.com <hackromanux@gmail.com@851d154f-5051-faa0-1c04-ff979f6fe470>
2012-08-20 05:24:21 +0000

    Anderson

--- trunk/ProyectoBolsaTrabajo/src/com/proyecto/negocio/GestionPostulante.java
+++ trunk/ProyectoBolsaTrabajo/src/com/proyecto/negocio/GestionPostulante.java
@@ -3,8 +3,35 @@ package com.proyecto.negocio;
 import com.proyecto.modelo.Postulante;
 
 public class GestionPostulante {
+	//Como postulante quiero registrarme en la bolsa de trabajo
 	public void registrarPostulante(String nombre, String paterno) {
 		Postulante p = new Postulante("Anderson");
+	}
+	
+	// como Postulantede quiero ingresar mis datos 
+	public void ingresarDatosPostulante(){
+		
+	}
+	//Como postulante quiero actualizar mi hoja de vida para las empresas.
+	public void actualizarDatosPostulante(){
+		
+	}
+	public void consultarAvisos(){
+		
+	}
+	
+	//Como postulante quiero poder consultar los avisos disponibles 
+	public void postularAvisos(){
+		
+	}
+	
+	//Como postulante deseo saber cuáles y cuantas postulaciones he realizado
+	public void consultarPostulaciones(){
+		
+	}
+	
+	public void actualizardatosPostulante(){
+		
 	}
 
 }
