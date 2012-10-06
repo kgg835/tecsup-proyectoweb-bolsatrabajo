cbe8bda681f9c15f133d44dab71c33ad74625b4e
ricardito_12_23@hotmail.com <ricardito_12_23@hotmail.com@851d154f-5051-faa0-1c04-ff979f6fe470>
2012-10-06 19:02:26 +0000

    servlets

--- trunk/ProyectoBolsaTrabajo/src/com/proyecto/test/testofertante.java
+++ trunk/ProyectoBolsaTrabajo/src/com/proyecto/test/testofertante.java
@@ -17,7 +17,7 @@ public class testofertante {
 		// fail("Not yet implemented");
 	}
 
-	@Test
+	//@Test
 	public void insertartest(){
 		Gestionofertante negocio= new Gestionofertante();
 		try{
@@ -34,7 +34,7 @@ public class testofertante {
 
 		Gestionofertante nego= new Gestionofertante();
 		try{
-			Ofertante x=nego.obtener(2);
+			Ofertante x=nego.obtener(5);
 			System.out.println(x.getCorreo());
 	
 		}catch(DAOExcepcion e)
@@ -57,20 +57,25 @@ public class testofertante {
 		}
 	}
 
-	//@Test
+	@Test
 	public void listar(){
 
 		Gestionofertante nego= new Gestionofertante();
 		try{
 			Collection<Ofertante> listado=nego.listar();
 	for (Ofertante ofertante : listado) {
-		System.out.println(ofertante.getRazonsocial());
+		Ofertante respuesta=new Ofertante();
+
+
+		System.out.println(ofertante.getRuc()+ofertante.getRazonsocial()+ofertante.getCorreo()+ofertante.getDescempresa()+ofertante.getUsuario()+ofertante.getPassword());
+		
 	}
 		}catch(DAOExcepcion e)
 		{
 			Assert.fail("fallo"+ e.getMessage());
 		}
 	}
+	//@Test
 	public void previsualizarDatosDelPostulante() {
 		@SuppressWarnings("unused")
 		GestionPostulante obj = new GestionPostulante();
