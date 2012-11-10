b470062563ff4d6aaaa604177c419c1262e1fed3
ricardito_12_23@hotmail.com <ricardito_12_23@hotmail.com@851d154f-5051-faa0-1c04-ff979f6fe470>
2012-11-10 14:06:06 +0000

    aviso service

--- BolsaTrabajoWeb/src/com/proyecto/negocio/service/AvisoService.java
+++ BolsaTrabajoWeb/src/com/proyecto/negocio/service/AvisoService.java
@@ -1,9 +1,14 @@
 package com.proyecto.negocio.service;
 
+import java.util.Collection;
+
 import com.proyecto.exception.DAOExcepcion;
 import com.proyecto.modelo.Aviso;
+import com.proyecto.modelo.HistorialPostulaciones;
 
 
 public interface AvisoService {
 	public Aviso insertar(Aviso vo) throws DAOExcepcion ;
+	public Collection<Aviso> listarAvisos() throws DAOExcepcion;
+
 }
