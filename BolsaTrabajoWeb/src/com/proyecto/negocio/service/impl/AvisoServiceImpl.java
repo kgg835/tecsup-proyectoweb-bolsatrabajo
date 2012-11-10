b7ccc5c2946e14c7b8921825648263dbb7d42f3c
ricardito_12_23@hotmail.com <ricardito_12_23@hotmail.com@851d154f-5051-faa0-1c04-ff979f6fe470>
2012-11-10 14:06:31 +0000

    serviceimpl

--- BolsaTrabajoWeb/src/com/proyecto/negocio/service/impl/AvisoServiceImpl.java
+++ BolsaTrabajoWeb/src/com/proyecto/negocio/service/impl/AvisoServiceImpl.java
@@ -1,5 +1,7 @@
 package com.proyecto.negocio.service.impl;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,10 +9,10 @@ import org.springframework.stereotype.Service;
 
 import com.proyecto.exception.DAOExcepcion;
 import com.proyecto.modelo.Aviso;
+import com.proyecto.modelo.HistorialPostulaciones;
 import com.proyecto.modelo.Postulante;
 import com.proyecto.negocio.service.AvisoService;
 import com.proyecto.negocio.service.PostulanteService;
-import com.proyecto.persistencia.dao.PostulanteDAO;
 import com.proyecto.persistencia.dao.avisoDAO;
 
 @Service
@@ -26,6 +28,11 @@ public class AvisoServiceImpl implements AvisoService{
 			return AvisoDAO.insertar(vo);
 		}
 		
+		
+		@Override
+		public Collection<Aviso> listarAvisos() throws DAOExcepcion{
+			return AvisoDAO.listarAvisos();
+		}
 }
 
 
