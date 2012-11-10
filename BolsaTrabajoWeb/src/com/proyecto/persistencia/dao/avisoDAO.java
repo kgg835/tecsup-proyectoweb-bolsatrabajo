d630343cc845959626cfa9fb3f2e62ef3be25811
ricardito_12_23@hotmail.com <ricardito_12_23@hotmail.com@851d154f-5051-faa0-1c04-ff979f6fe470>
2012-11-10 14:06:45 +0000

    dd

--- BolsaTrabajoWeb/src/com/proyecto/persistencia/dao/avisoDAO.java
+++ BolsaTrabajoWeb/src/com/proyecto/persistencia/dao/avisoDAO.java
@@ -1,9 +1,12 @@
 package com.proyecto.persistencia.dao;
 
+import java.util.Collection;
+
 import com.proyecto.exception.DAOExcepcion;
 import com.proyecto.modelo.Aviso;
-import com.proyecto.modelo.Usuario;
 
 public interface avisoDAO {
 	public Aviso insertar(Aviso vo) throws DAOExcepcion;
+	public Collection<Aviso> listarAvisos() throws DAOExcepcion;
+
 }
